085427057cad1c192a4a79d1e2c3d09f24635699
Charlie <charln2@uw.edu>
2018-02-16 17:46:38 -0800

    FIX PIVOT ROTATION BUG! :D

--- app/src/main/java/com/charln2/crochendo/MainActivity.java
+++ app/src/main/java/com/charln2/crochendo/MainActivity.java
@@ -1,9 +1,14 @@
 package com.charln2.crochendo;
 
+import android.graphics.drawable.VectorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -11,6 +16,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
     private static final String placeholderWebsite = "https://hooked-on-crafting.com/2013/09/10/bellflower-infinity-scarf-free-pattern/";
@@ -23,6 +29,34 @@ public class MainActivity extends AppCompatActivity {
 
         result = (TextView) findViewById(R.id.result_text_view);
         fetchWebsite(placeholderWebsite);
+
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.rel);
+        View dc1 = getLayoutInflater().inflate(R.layout.dc, null);
+        View dc2 = getLayoutInflater().inflate(R.layout.dc, null);
+        View dc3 = getLayoutInflater().inflate(R.layout.dc, null);
+        View dc4 = getLayoutInflater().inflate(R.layout.dc, null);
+        View dc5 = getLayoutInflater().inflate(R.layout.dc, null);
+
+        rl.addView(dc1);
+        rl.addView(dc2);
+        rl.addView(dc3);
+        rl.addView(dc4);
+        rl.addView(dc5);
+
+//        rl.getChildAt(0).
+        rl.getChildAt(0).setRotation(45);
+//        rl.getChildAt(1).setRotation(-35);
+//        rl.getChildAt(3).setRotation(35);
+//        rl.getChildAt(4).setRotation(70);
+
+//        shell.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+//        shell.setPivotX();
+//        shell.setPivotY(0f);
+//        shell.setPivotY(0);
+//        shell.setPivotX(0);
+//        Toast.makeText(MainActivity.this, String.format("(%f, %f, %d)", shell.getPivotX(), shell.getPivotY(), shell.getWidth()), Toast.LENGTH_SHORT).show();
+//        shell.setRotation(70);
+
     }
 
     void fetchWebsite(final String url) {
