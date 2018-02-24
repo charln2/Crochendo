e65ce29e395eb594ecc83a962122aeaf36d173f0
Charlie Nguyen <charln2@uw.edu>
2018-02-23 19:32:00 -0800

    Improve parsing by excluding comma delimiters within ()s

--- app/src/main/java/com/charln2/crochendo/Pattern.java
+++ app/src/main/java/com/charln2/crochendo/Pattern.java
@@ -7,6 +7,7 @@ import com.charln2.crochendo.Instructions.Instruction;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -25,12 +26,12 @@ public class Pattern {
         add("dc");
         add("sl st");
     }};
-    Stitch head, tail, x;
-    ArrayList<Stitch> rows;
+    private Stitch head, tail, x;
+    private ArrayList<Stitch> rows;
     private static boolean newRow = false;
     private boolean rsIsOdd = true;
     private Queue<Instruction> q = new LinkedList<>();
-    // make a queue
+    HashMap<String, Stitch> hold = new HashMap<>();
 
     // default constructor for testing purposes
     public Pattern() {
@@ -80,15 +81,54 @@ public class Pattern {
 
     void parseLine(String line) {
         Scanner sc = new Scanner(line);
-        sc.useDelimiter("[,:;.—]");
+        // commas note enclosed in parentheses or matching list of specific known delimiters
+        sc.useDelimiter("(,(?![^()]*+\\)))|[:;.—]");
         //<stitch, times, in x, note>
 
+        StringBuilder sb = new StringBuilder();
+        ArrayList<String> parsedInstructions = new ArrayList<>();
         while (sc.hasNext()) {
-            Instruction inst = InstructionFactory.getInstruction(sc.next());
-            if (inst != null) {
-                q.add(inst);
+            while (sb.length() <= 0 || !isValidInstruction(sb.toString())) {
+                sb.append(sc.next());
             }
+            // additional unpacking of special-case instruction line fragments
+            // HOLD: *[some stitch]
+                // map * stitch position, continue with rest of stitch
+            if (sb.toString().startsWith("*")) {
+                parsedInstructions.add(sc.next("\\*"));
+                sb.deleteCharAt(0);
+                continue;
+            }
+
+            // "(": 2 cases
+            // SUB-INSTRUCTION/ COMMENT: stitch (...counts as...)
+                // queue one, then the other.
+
+            // SHELL: if startsWith (, group, skip, continue to next ,.
+            // (stitches) in next...,
+                // append remaining until ), then remaining instruction up to ,.
+                // shell aggregates many stitches, uses remaining [...in next] instruction.
+                // behaves like a regular stitch, but groups instructions
+
+
+            // queue 1+ instructions
+            for (String s : parsedInstructions) {
+                Instruction inst = InstructionFactory.getInstruction(s);
+                if (inst != null) {
+                    q.add(inst);
+                }
+            }
+//            parsedInstructions.clear();
         }
+    }
+
+    private boolean isValidInstruction(String s) {
+        HashSet<Character> validStarts = new HashSet<Character>() {{
+            add('*');
+            add('(');
+        }};
+
+        return stitches.contains(s) || validStarts.contains(s.charAt(0));
     }
 
     public void append(Stitch st) {
--- app/src/test/java/com/charln2/crochendo/InstructionParsingUnitTest.java
+++ app/src/test/java/com/charln2/crochendo/InstructionParsingUnitTest.java
@@ -2,7 +2,11 @@ package com.charln2.crochendo;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class InstructionParsingUnitTest {
     @Test
@@ -17,7 +21,7 @@ public class InstructionParsingUnitTest {
         p.parseLine("ch 5");
         p.executeInstructions();
 
-        assertEquals(p.toString(), "sl st|ch   |ch   |ch   |ch   |ch   ");
+        assertEquals("sl st|ch   |ch   |ch   |ch   |ch   ", p.toString());
     }
 
     @Test
@@ -30,4 +34,47 @@ public class InstructionParsingUnitTest {
         p.executeInstructions();
         assertTrue(p.isNewRow());
     }
+
+    @Test
+    public void hold() throws Exception {
+//        Scanner sc = new Scanner("*stitch with asterisk");
+//        StringBuilder sb = new StringBuilder("*stitch with asterisk");
+//        sb.replace(0,1, "");
+//        System.out.println(sb.toString());
+    }
+    // --- Small Experiments ---
+
+    @Test
+    public void scanner() throws Exception {
+        Scanner sc = new Scanner("Some text (with text in braces),");
+        String braces = sc.findInLine("\\(.*\\)");
+        System.out.println(braces.replaceAll("[()]", ""));
+        assertEquals("(with text in braces)", braces);
+
+        sc = new Scanner("text without braces");
+        String mystery = sc.findInLine("\\(.*\\)");
+        System.out.println(mystery);
+    }
+    @Test
+    public void scan() throws Exception {
+        String str = "Ch 32.\n" +
+                "Row 1 (RS): Dc in 4th ch from hook (beginning ch counts as dc), *sk next 2 ch, (3 dc, ch 1, 3 dc) in next ch, sk next 2 ch, dc in next 2 ch; repeat from * across, turn—4 pattern repeats.\n" +
+                "Row 2: Ch 3 (counts as dc here and throughout), sk first dc, dc in next dc, *sk next 2 dc, dc in next dc, ch 1, (dc, ch 1, dc) in next ch-1 sp, ch 1, dc in next dc, sk next 2 dc, dc in next 2 dc; repeat from * across, turn.\n" +
+                "Row 3: Ch 3, sk first dc, dc in next dc, *sk next ch-1 sp, (2 dc, ch 3, 2 dc) in next ch-1 sp, sk next ch-1 sp, sk next dc, dc in next 2 dc; repeat from * across, turn.\n" +
+                "                                                                 \n" +
+                "Row 4: Ch 3, sk first dc, dc in next dc, *(3 dc, ch 1, 3 dc) in next ch-3 sp, sk next 2 dc, dc in next 2 dc; repeat from * across, turn.\n" +
+                "Repeat Rows 2–4 until piece measures about 58″/147.5cm. Do not fasten off.";
+        Scanner sc = new Scanner(str);
+        // commas note enclosed in parentheses or matching list of specific known delimiters
+        sc.useDelimiter("(,(?![^()]*+\\)))|[:;.—]");
+        while (sc.hasNext()) {
+            System.out.println(sc.next());
+        }
+    }
+
+
+
+
+
+
 }
